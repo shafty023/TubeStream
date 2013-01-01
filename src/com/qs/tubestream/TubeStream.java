5a40145ccee67596c48926214f8503b52d993d0a
Daniel Ochoa <gameboy45@gmail.com>
2013-01-01 15:14:54 -0600

    * Created skeleton TubeStream application class

---
+++ src/com/qs/tubestream/TubeStream.java
@@ -0,0 +1,14 @@
+package com.qs.tubestream;
+
+import android.app.Application;
+
+public class TubeStream extends Application {
+    public static final String LOG_TAG = "TubeStream";
+
+    public static boolean DEBUG = true;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
